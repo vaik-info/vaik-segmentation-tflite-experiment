17bed370be9f0b82108310c6e6ba6d29d06ca6ef
vaik-info <info@vaikobo.com>
2023-03-04 22:59:45 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,119 @@
-# vaik-segmentation-tflite-experiment
+# vaik-segmentation-pb-experiment
+
+Create json file by segmentation model. Calc mIoU.
+
+![draw](https://user-images.githubusercontent.com/116471878/222900493-a2606e90-58c7-427e-9ae2-7d05b3e74e6b.png)
+
+
+## Install
+
+```shell
+pip install -r requirements.txt
+```
+
+## Usage
+
+### Create json file
+
+```shell
+python inference.py --input_saved_model_dir_path '~/output_model/2023-03-04-19-07-35/step-5000_batch-8_epoch-3_loss_0.0017_one_hot_mean_io_u_0.8605_val_loss_0.1085_val_one_hot_mean_io_u_0.1840' \
+                --input_classes_path './test_images/classes.txt' \
+                --input_image_dir_path './test_images/raw' \
+                --answer_image_dir_path './test_images/seg' \
+                --output_dir_path '~/.vaik-segmentation-pb-experiment/test_images_out'
+```
+
+- input_image_dir_path
+    - example
+
+```shell
+.
+├── 000000005.png
+├── 000000011.png
+├── 000000017.png
+├── 000000023.png
+├── 000000029.png
+├── 000000035.png
+├── 000000041.png
+├── 000000047.png
+├── 000000053.png
+├── 000000059.png
+├── 000000065.png
+├── 000000071.png
+├── 000000077.png
+├── 000000083.png
+├── 000000089.png
+└── 000000095.png
+
+```
+
+#### Output
+- output_json_dir_path
+    - example
+
+```json
+{
+  "answer": {
+    "array": [
+      0,
+      ・・・
+      0
+    ],
+    "shape": [
+      320,
+      320
+    ]
+  },
+  "labels": {
+    "array": [
+      0,
+      ・・・
+      0
+    ],
+    "shape": [
+      320,
+      320
+    ]
+  }
+}
+```
+-----
+
+### Calc mIoU
+
+```shell
+python calc_miou.py --input_json_dir_path '~/.vaik-segmentation-pb-experiment/test_images_out' \
+                --input_classes_path './test_images/classes.txt' \
+                --ignore_label 'background'
+```
+
+#### Output
+
+``` text
+mIoU:0.9512
+background: 0.9987
+zero: 0.9434
+one: 0.9468
+two: 0.9606
+three: 0.9668
+four: 0.9388
+five: 0.9712
+six: 0.9411
+seven: 0.9377
+eight: 0.9557
+nine: 0.9502
+```
+
+-----
+
+### Draw
+
+```shell
+python draw.py --input_json_dir_path '~/.vaik-segmentation-pb-experiment/test_images_out' \
+                --input_classes_path './test_images/classes.txt' \
+                --output_dir_path '~/.vaik-segmentation-pb-experiment/test_images_out_draw'
+```
+
+#### Output
+
+![draw](https://user-images.githubusercontent.com/116471878/222900493-a2606e90-58c7-427e-9ae2-7d05b3e74e6b.png)
---
+++ inference.py
@@ -0,0 +1,71 @@
+import argparse
+import os
+import glob
+import json
+import tqdm
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from vaik_segmentation_tflite_inference.tflite_model import TfliteModel
+
+
+def main(input_saved_model_path, input_classes_path, input_image_dir_path, answer_image_dir_path, output_dir_path):
+    os.makedirs(output_dir_path, exist_ok=True)
+    with open(input_classes_path, 'r') as f:
+        classes = f.readlines()
+    classes = tuple([label.strip() for label in classes])
+
+    model = TfliteModel(input_saved_model_path, classes)
+
+    types = ('*.jpg', '*.jpeg', '*.png', '*.JPG', '*.JPEG', '*.PNG')
+    image_path_list = []
+    for file in types:
+        image_path_list.extend(glob.glob(os.path.join(input_image_dir_path, f'{file}'), recursive=True))
+    image_list = []
+    for image_path in tqdm(image_path_list, 'read images'):
+        image = np.asarray(Image.open(image_path).convert('RGB'))
+        image_list.append(image)
+
+    import time
+    total_time = 0
+    output_list = []
+    for image in tqdm(image_list, desc='inference'):
+        start = time.time()
+        output, raw_pred = model.inference(image)
+        end = time.time()
+        total_time += end - start
+        output_list.append(output)
+
+    for image_path, output_elem in zip(image_path_list, output_list):
+        answer_image = np.asarray(
+            Image.open(os.path.join(answer_image_dir_path, os.path.basename(image_path).replace('raw', 'seg'))).convert(
+                'L'))
+        output_json_path = os.path.join(output_dir_path, os.path.splitext(os.path.basename(image_path))[0] + '.json')
+        output_elem['answer'] = {'array': answer_image.flatten().tolist(), 'shape': answer_image.shape}
+        output_elem['labels'] = {'array': output_elem['labels'].flatten().tolist(),
+                                 'shape': output_elem['labels'].shape}
+        with open(output_json_path, 'w') as f:
+            json.dump(output_elem, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+    print(f'{len(image_list) / (end - start)}[images/sec]')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='inference')
+    parser.add_argument('--input_saved_model_path', type=str,
+                        default='~/.vaik-segmentation-tflite-exporter/model.tflite')
+    parser.add_argument('--input_classes_path', type=str,
+                        default=os.path.join(os.path.dirname(__file__), 'test_images/classes.txt'))
+    parser.add_argument('--input_image_dir_path', type=str,
+                        default=os.path.join(os.path.dirname(__file__), 'test_images/raw'))
+    parser.add_argument('--answer_image_dir_path', type=str,
+                        default=os.path.join(os.path.dirname(__file__), 'test_images/seg'))
+    parser.add_argument('--output_dir_path', type=str, default='~/.vaik-segmentation-tflite-experiment/test_images_out')
+    args = parser.parse_args()
+
+    args.input_saved_model_path = os.path.expanduser(args.input_saved_model_path)
+    args.input_classes_path = os.path.expanduser(args.input_classes_path)
+    args.input_image_dir_path = os.path.expanduser(args.input_image_dir_path)
+    args.answer_image_dir_path = os.path.expanduser(args.answer_image_dir_path)
+    args.output_dir_path = os.path.expanduser(args.output_dir_path)
+
+    main(**args.__dict__)
